1ce9df02bb0558d021c15e286a535954d6bf1747
Alan <alanalberino@gmail.com>
2020-12-15 13:08:05 -0300

    Correccion en websocket

--- src/main/java/com/edu/iua/WebSocketConfig.java
+++ src/main/java/com/edu/iua/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.edu.iua;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,12 +10,6 @@ import com.edu.iua.rest.Constantes;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry registry) {
-		registry.enableSimpleBroker("/iw3"); 
-		registry.setApplicationDestinationPrefixes("/wsin"); 
-	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
